a6bc688368549c49461fe18feb25d7ae50efadd6
MrRetro <caijiajia1@126.com>
2018-02-11 16:26:52 +0800

    jq基本方法

--- js/jq1.0.js
+++ js/jq1.0.js
@@ -147,6 +147,16 @@
     // 给jQuery添加一些静态方法
     jQuery.extend({
     	
+    	// 获取样式,已经处理了兼容性
+    	getStyle: function( dom, style ){
+    		
+    		//优先判断支不支持现代样式获取方式
+    		if( window.getComputedStyle){
+    			return window.getComputedStyle( dom )[ style ];
+    		}else{
+    			return dom.currentStyle[ style ];
+    		}
+    	},
         // 遍历对象或类数组
         each: function( obj, fn ) {
             // 如果是数组或伪数组，按照顺序遍历
@@ -747,7 +757,7 @@
 	    },
 	    
 	    // 给所有元素添加html内容,或者其他元素
-	    append:function( context ){
+	    append: function( context ){
 	    	
 	    	/*
 	    	 * 实现思路:
@@ -778,7 +788,7 @@
 	    },
 	    
 	    // 给所有元素添加html内容,或者其他元素
-	    _append:function( context ){
+	    _append: function( context ){
 	    	
 	    	/*
 	    	 * 实现思路:
@@ -811,7 +821,7 @@
 	    },
 	    
 	    // 给所有元素内容前添加html内容,或者其他元素
-	    prepend:function( context ){
+	    prepend: function( context ){
 	    	var $context = $( context );
 	    	
 	    	// 如果是字符串,则累加给所有元素
@@ -830,7 +840,7 @@
 	    	return this;
 	    },
 	    // 给所有元素内容前添加html内容,或者其他元素
-	    _prepend:function( context ){
+	    _prepend: function( context ){
 	    	var $context = $( context );
 	    	
 	    	// 如果是字符串,则累加给所有元素
@@ -847,6 +857,521 @@
 	    	}
 	    	
 	    	return this;
+	    },
+	    //给所有元素添加新的class
+	    addClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次判断每一个元素有没有要添加的className
+	    	 * 3、有则忽略,没有则添加( className += ' ' + classNames )
+	    	 * 4、考虑trim一下
+	    	 * 5、链式编程返回this
+	    	 **/
+	    	
+	    	for( var i = 0, len = this.length; i < len; i++ ){
+	    		
+	    		if((' ' + this[ i ].className + ' ').indexOf(' ' + className + ' ') == -1){
+	    			this[ i ].className+=' '+ className;
+	    		}
+	    		
+	    	}
+	    	
+	    	//链式编程返回this
+	    	return this;
+	    	
+	    },
+	    //给所有元素添加新的class
+	    _addClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次判断每一个元素有没有要添加的className
+	    	 * 3、有则忽略,没有则添加( className += ' ' + classNames )
+	    	 * 4、考虑trim一下
+	    	 * 5、链式编程返回this
+	    	 **/
+	    	
+	    	this.each(function(){
+	    		if((' ' + this.className + ' ').indexOf(' ' + className + ' ') == -1){
+	    			this.className+=' '+ className;
+	    		}
+	    	})
+	    	
+	    	//链式编程返回this
+	    	return this;
+	    	
+	    },
+	    //给所有元素添加新的class
+	    __addClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次判断每一个元素有没有要添加的className
+	    	 * 3、有则忽略,没有则添加( className += ' ' + classNames )
+	    	 * 4、考虑trim一下
+	    	 * 5、链式编程返回this
+	    	 **/
+	    	
+	    	this.each(function(){
+	    		if( !jQuery( this ).hasClass( className ) ){
+	    			this.className+=' '+ className;
+	    		}
+	    	})
+	    	
+	    	//链式编程返回this
+	    	return this;
+	    	
+	    },
+	    //删除所有元素指定的class
+	    removeClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、没有参数,遍历所有的元素, 设置他们的className为空
+	    	 * 2、有参数,遍历所有的元素,删除指定的className(元素.className.replace())
+	    	 * 把指定的className替换为空格,最后整体trim一下
+	    	 * 3、链式编程返回this
+	    	 **/
+	    	
+	    	if( arguments.length === 0 ){
+	    		this.each( function(){
+	    			this.className = '';
+	    		});
+	    	}else{
+	    		this.each( function(){
+	    			this.className = (' ' + this.className + ' ').replace(' ' + className +' ', ' ');
+	    		});
+	    	}
+	    	
+	    },
+	    //判断所有元素有没有指定的class
+	    hasClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次获取每一个元素的className,为了方便判断,首尾加空格
+	    	 * 3、利用处理过的className字符串的indexOf方法判断有没有指定的className(这个class类名)
+	    	 * 4、如果有一个元素的判断结果不为-1,就返回true
+	    	 * 5、否则返回false。
+	    	 **/
+	    	
+	    	for( var i = 0, len = this.length; i < len; i++ ){
+	    		
+	    		if( (' ' + this[ i ].className + ' ').indexOf(' ' + className + ' ') > -1){
+	    			return true;
+	    		}
+	    	}
+	    	
+	    	//如果所有的元素都没有 返回false
+	    	return false;
+	    	
+	    },
+	    //判断所有元素有没有指定的class
+	    _hasClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次获取每一个元素的className,为了方便判断,首尾加空格
+	    	 * 3、利用处理过的className字符串的indexOf方法判断有没有指定的className(这个class类名)
+	    	 * 4、如果有一个元素的判断结果不为-1,就返回true
+	    	 * 5、否则返回false。
+	    	 **/
+	    	
+	    	//默认为所有的元素都没有指定的className
+	    	var has = false;
+	    	
+	    	this.each( function(){
+				
+				// 只要有一个元素存在指定的className,那么就修改has的变量值为true
+				if( (' ' + this.className + ' ').indexOf(' ' + className + ' ') > -1 ){
+					has = true;
+				}
+				
+	    	});
+	    	
+	    	//返回has
+	    	return has;
+	    	
+	    },
+	    //判断所有元素有没有指定的class
+	    __hasClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、依次获取每一个元素的className,为了方便判断,首尾加空格
+	    	 * 3、利用处理过的className字符串的indexOf方法判断有没有指定的className(这个class类名)
+	    	 * 4、如果有一个元素的判断结果不为-1,就返回true
+	    	 * 5、否则返回false。
+	    	 **/
+	    	
+	    	//默认为所有的元素都没有指定的className
+	    	var has = false;
+	    	
+	    	this.each( function(){
+				
+				// 只要有一个元素存在指定的className,那么就修改has的变量值为true
+				if( (' ' + this.className + ' ').indexOf(' ' + className + ' ') > -1 ){
+					has = true;
+					
+					// 中断遍历
+					return false;
+				}
+				
+	    	});
+	    	
+	    	//返回has
+	    	return has;
+	    	
+	    },
+	    //有则删除,没有则添加
+	    toggleClass: function( className ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、遍历所有的元素
+	    	 * 2、判断每一个元素有则删除,没有添加
+	    	 **/
+	    	
+	    	this.each( function(){
+	    		// 这里的this是遍历到每一个原生DOM
+	    		// 先包装成JQ对象,就可以复用之前写好的方法了
+	    		var $this = jQuery( this );
+				
+				// 有则删除,没有则添加
+				if( $this.hasClass( className ) ){
+					$this.removeClass( className );
+				}else{
+					$this.addClass( className );
+				}
+				
+	    	})
+	    },
+	    //设置或者获取元素属性节点值
+	    attr: function( attr, val ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、判断attr是不是字符串或者对象,不是直接return this
+	    	 * 2、如果是字符串,那么继续判断arguments的length
+	    	 * 3、length为1,则获取第一个元素指定的属性节点值返回
+	    	 * 4、length>=2,遍历所有元素,分别给他们设置新的属性节点值( setAttribute )
+	    	 * 5、如果不是字符串(是对象),那么遍历这个对象,得到所有的属性节点值,
+	    	 * 然后遍历所有的元素,把所有的属性节点分别添加到这些元素中
+	    	 * 6、return this
+	    	 **/
+	    	
+	    	// 如果不是字符串也不是对象
+	    	if( !jQuery.isString( attr ) && !jQuery.isObject( attr ) ){
+	    		return this;
+	    	}
+	    	// 如果是字符串
+	    	if( jQuery.isString( attr ) ){
+	    		
+	    		// length为1,则获取第一个元素指定的属性节点值返回
+	    		if( arguments.length === 1 ){
+	    			return this.get( 0 ).getAttribute( attr );
+	    		}
+	    		
+	    		// 如果length为多个(2及2个以上)
+	    		// 则遍历所有的元素,分别设置属性节点值
+	    		else{
+	    			for( var i = 0, len = this.length; i < len; i++){
+	    				this[ i ].setAttribute( attr, val );
+	    			}
+	    		}
+	    		
+	    	}
+	    	
+	    	// 如果是对象
+	    	// 遍历这个对象,和所有的元素, 分别添加遍历到的属性接节点值
+	    	else{
+	    		
+	    		for( var key in attr ){
+	    			for( var i = 0, len = this.length; i < len; i++ ){
+	    				this[i].setAttribute( key, attr[key] )
+	    			}
+	    		}
+	    		
+	    	}
+	    	return this;
+	    	
+	    },
+	    //设置或者获取元素属性节点值
+	    _attr: function( attr, val ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、判断arguments.length
+	    	 * 2、如果为1
+	    	 * 3、继续判断attr是不是字符串,如果是获取第一个元素指定的属性节点值返回
+	    	 * 4、如果不是继续判断是不是对象,如果是遍历这个对象,得到所有属性节点值,
+	    	 * 然后遍历所有的元素,把所有的属性节点分别添加到这些元素中
+	    	 * 5、如果为多个(2及以上),遍历所有元素分别设置属性节点值
+	    	 * 6、return this
+	    	 **/
+	    	
+	    	// 把实例使用别称存储一下，为了在别的地方使用
+	    	var self = this;
+	    	
+	    	if( arguments.length === 1 ){
+	    		
+	    		// 如果是字符串,获取第一个元素指定的属性节点值返回
+	    		if( jQuery.isString( attr ) ){
+	    			return this[ 0 ].getAttribute( attr );
+	    		}
+	    		
+	    		// 如果是对象,把对象中所有的属性节点添加到所有的元素中
+	    		else if( jQuery.isObject( attr) ){
+	    			
+	    			// 使用jQ静态each方法遍历attr对象
+	    			jQuery.each( attr, function( key, val ){
+	    				// 这里便利到的val不是对象类型, 是基本数据类型
+	    				// 我们要使用的就是基本数据类型, 而this是基本数据类型的包装类型(),
+	    				// 所以这里不能使用this。
+	    				
+	    				//遍历所有的元素
+	    				self.each(function(){
+	    					
+	    					// 给遍历到的每一个元素分别设置外面遍历到的属性节点
+	    					this.setAttribute( key, val );
+	    					
+	    				});
+	    				
+	    			});
+	    			
+	    		}
+	    	//如果参数个数为多个
+	    	}else if( arguments.length >= 2){
+	    		
+	    		// 遍历所有元素分别设置对应的属性节点值
+	    		this.each(function(){
+	    			this.setAttribute( attr, val )
+	    		});
+	    		
+	    	}
+	    	
+	    	// 如果不是字符串也不是对象
+	    	if( !jQuery.isString( attr ) && !jQuery.isObject( attr ) ){
+	    		return this;
+	    	}
+	    	
+	    	return this;
+	    	
+	    },
+	    //设置或者获取元素的属性值
+	    prop: function( attr, val ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、判断attr是不是字符串或者对象,不是直接return this
+	    	 * 2、如果是字符串,那么继续判断arguments的length
+	    	 * 3、length为1,则获取第一个元素指定的属性节点值返回
+	    	 * 4、length>=2,遍历所有元素,分别给他们设置新的属性节点值( setAttribute )
+	    	 * 5、如果不是字符串(是对象),那么遍历这个对象,得到所有的属性节点值,
+	    	 * 然后遍历所有的元素,把所有的属性节点分别添加到这些元素中
+	    	 * 6、return this
+	    	 **/
+	    	
+	    	// 如果不是字符串也不是对象
+	    	if( !jQuery.isString( attr ) && !jQuery.isObject( attr ) ){
+	    		return this;
+	    	}
+	    	
+	    	// 如果是字符串
+	    	if( jQuery.isString( attr ) ){
+	    		
+	    		// 如果只有一个参数为字符串,那么返回第一个元素指定的属性值
+	    		if( arguments.length === 1 ){
+	    			return this[0][attr];
+	    		}
+	    		
+	    		// 如果有多个参数, 那么给所有元素设置指定的属性值
+	    		else{
+	    			for( var i = 0, len = this.length; i < len; i++){
+	    				this[ i ][ attr ] = val;
+	    			}
+	    		}
+	    		
+	    	}
+	    	
+	    	// 如果是对象
+	    	// 遍历这个对象,和所有的元素, 分别添加遍历到的属性值
+	    	else{
+	    		
+	    		for( var key in attr ){
+	    			for( var i = 0, len = this.length; i < len; i++ ){
+	    				
+	    				// 给每一个元素设置属性
+	    				this[ i ][ key ] = attr[ key ];
+	    			}
+	    		}
+	    		
+	    	}
+	    	return this;
+	    	
+	    },
+	    //设置或者获取属性值
+	    _prop: function( attr, val ){
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、判断arguments.length为1
+	    	 * 2、继续判断attr是不是字符串,如果是获取第一个元素指定的属性
+	    	 * 3、如果不是继续判断是不是对象,如果是遍历这个对象,得到所有属性,
+	    	 * 4、如果为多个(2及以上),遍历所有元素分别设置指定属性
+	    	 * 5、链式编程return this
+	    	 **/
+	    	
+	    	// 把实例使用别称存储一下，为了在别的地方使用
+	    	var self = this;
+	    	
+	    	if( arguments.length === 1 ){
+	    		
+	    		// 如果是字符串,获取第一个元素指定的属性节点值返回
+	    		if( jQuery.isString( attr ) ){
+	    			return this[ 0 ][ attr ];
+	    		}
+	    		
+	    		// 如果是对象,把对象中所有的属性添加到所有的元素中
+	    		else if( jQuery.isObject( attr) ){
+	    			
+	    			// 使用jQ静态each方法遍历attr对象
+	    			jQuery.each( attr, function( key, val ){
+	    				// 这里便利到的val不是对象类型, 是基本数据类型
+	    				// 我们要使用的就是基本数据类型, 而this是基本数据类型的包装类型(),
+	    				// 所以这里不能使用this。
+	    				
+	    				//遍历所有的元素
+	    				self.each(function(){
+	    					
+	    					// 给遍历到的每一个元素分别设置外面遍历到的属性
+	    					this[ key ] = val;
+	    					
+	    				});
+	    				
+	    			});
+	    			
+	    		}
+	    	//如果参数个数为多个
+	    	}else if( arguments.length >= 2){
+	    		
+	    		// 遍历所有元素分别设置对应的属性值
+	    		this.each(function(){
+	    			this[ attr ] = val;
+	    		});
+	    		
+	    	}
+	    	
+	    	return this;
+	    	
+	    },
+	    //获取或者设置元素的value的属性值
+	    val: function( value ){
+	    	
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、如果arguments.length === 0,则直接返回第一个元素的value属性值
+	    	 * 2、否则,遍历所有的元素,分别设置对应的value属性值
+	    	 * 3、链式编程返回this
+	    	 **/
+	    	
+	    	// 没有传参,返回第一个元素的value属性值
+	    	if( arguments.length === 0 ){
+	    		return this[ 0 ].value;
+	    	}
+	    	
+	    	// 否则给所有元素分别设置value值
+	    	else {
+	    		for( var i = 0, len = this.length; i < len; i++){
+	    			this[ i ].value = value;
+	    		}
+	    	}
+	    	
+	    	//链式编程
+	    	return this;
+	    	
+	    },
+	    //获取或者设置元素的value的属性值
+	    _val: function( value ){
+	    	
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、如果arguments.length === 0,则直接返回第一个元素的value属性值
+	    	 * 2、否则,遍历所有的元素,分别设置对应的value属性值
+	    	 * 3、链式编程返回this
+	    	 **/
+	    	
+	    	// 没有传参,返回第一个元素的value属性值
+	    	if( arguments.length === 0 ){
+	    		return this[ 0 ].value;
+	    	}
+	    	
+	    	// 否则给所有元素分别设置value值
+	    	else {
+	    		this.each( function(){
+	    			this.value = value;
+	    		});
+	    	}
+	    	
+	    	//链式编程
+	    	return this;
+	    	
+	    },
+	    //获取或者设置元素的value的属性值
+	    __val: function( value ){
+	    	
+	    	// 没有传参,借用prop获取第一个元素的value属性值
+	    	if( arguments.length === 0 ){
+	    		return this.prop( 'value' );
+	    	}
+	    	// 传参了,借用prop给所有元素设置新的value属性值
+	    	else {
+	    		return this.prop( 'value', value );
+	    	}
+	    	
+	    },
+	    // 设置或者获取样式
+	    css: function( styleName, style ){
+	    	
+	    	/*
+	    	 * 实现思路:
+	    	 * 1、如果arguments.length为1
+	    	 * 2、那么判断styleName是不是字符串, 是则获取第一个元素指定的样式
+	    	 * 3、判断styleName是不是对象。是则遍历这个对象得到所有的样式分别设置给所有元素
+	    	 * 4、如果arguments.length>=2,遍历所有元素分别设置指定的样式
+	    	 * 5、链式变成返回this
+	    	 **/
+	    	
+	    	if( arguments.length === 1 ){
+	    		
+	    		if( jQuery.isString( styleName ) ){
+	    			return jQuery.getStyle( this[ 0 ], styleName );
+	    		}
+	    		
+	    		else if( jQuery.isObject( styleName ) ){
+	    			
+	    			// 遍历styleName得到所有的样式
+	    			for( var key in styleName ){
+	    				
+	    				// 遍历得到所有元素
+	    				for( var i = 0, len = this.length; i < len; i++ ){
+	    					
+	    					// 给素有的元素设置遍历到所有样式
+	    					this[ i ][ 'style' ][ key ] = styleName[ key ];
+	    					
+	    				}
+	    				
+	    			}
+	    		}
+	    		
+	    	}
+	    	
+	    	else if( arguments.length >= 2 ){
+	    		
+	    		//给所有元素设置指定样式
+	    		for( var i = 0, len = this.length; i <len; i++ ){
+	    			this[ i ][ 'style' ][ styleName ] = style;
+	    		}
+	    		
+	    	}
+	    	
+	    	return this;
+	    	
 	    }
 	} );
 
